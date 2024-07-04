44989a9a918b11f255b1da9327462c68522cc7b8
xhaqur xhariph <shakuritoshariff@gmail.com>
2024-07-04 16:10:28 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const remove = document.querySelector('main#main')
+remove.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id = 'victory';
+newHeader.textContent = 'Abdishakur is the champion'
+document.body.append(newHeader);
+
+
+
